30da8239b11c66581f0383a22c10c8ec64b86442
Pense.try <pensetry@hotmail.com>
2025-02-19 17:11:16 -0300

    Menu 1v loan corr

--- src/JS/cart.js
+++ src/JS/cart.js
@@ -107,7 +107,7 @@ export function toggleCarrinho() {
 }
 
 export function fazerPedido() {
-    const numeroWhatsApp = '55996145479';
+    const numeroWhatsApp = '5543996145479';
     let mensagem = 'Olá, gostaria de fazer o seguinte pedido:\n\n';
 
     carrinho.forEach(item => {
